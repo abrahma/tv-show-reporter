4a8ba3b2da7d59b30e0eb7f71c38d4a17cdd0f99
abrahma <49549385+abrahma@users.noreply.github.com>
2019-04-14 21:33:34 +0530

    Add files via upload

--- src/components/ShowDetails.js
+++ src/components/ShowDetails.js
@@ -11,52 +11,54 @@ function ShowDetailsModal(props){
     const {open,closeModal,details} = props;
 
     return(
-        <Modal classNames={{overlay: styles.ShowDetailsModalOverlay, modal: styles.ShowDetailsModal}} open={open} onClose={closeModal} center>
+        <Modal open={open} onClose={closeModal} center>
              <h2>{details.name}</h2>
-             <div className="DetailsCenter">
-                 <div className="DetailsImage">
-                    <img src={details.image ? details.image.original : null}/>
-                 </div>
-                 <div className="DetailsInsights">
-                        <div>
-                            <StarRatings
-                                     rating={parseInt(details.rating.average) > 0 ? details.rating.average/2 : 0}
-                                     starRatedColor="blue"
-                                     starDimension="30px"
-                                     starSpacing="5px"
-                                     numberOfStars={5}
-                                     name='showRating'
-                            />
-                            <TagViewer values={details.genres}></TagViewer>
-                        </div>
-                        <table className="InsightTable">
-                        <tbody>
-                            <tr>
-                                <td className="InsightHeader">Language</td>
-                                <td>{details.language}</td>
-                            </tr>
-                            </tbody>
+             <div className="ShowDetailsModal">
+                 <div className="DetailsCenter">
+                     <div className="DetailsImage">
+                        <img src={details.image ? details.image.original : null}/>
+                     </div>
+                     <div className="DetailsInsights">
+                            <div>
+                                <StarRatings
+                                         rating={parseInt(details.rating.average) > 0 ? details.rating.average/2 : 0}
+                                         starRatedColor="blue"
+                                         starDimension="30px"
+                                         starSpacing="5px"
+                                         numberOfStars={5}
+                                         name='showRating'
+                                />
+                                <TagViewer values={details.genres}></TagViewer>
+                            </div>
+                            <table className="InsightTable">
                             <tbody>
-                            <tr>
-                                <td className="InsightHeader">Official Site</td>
-                                <td><a href={details.officialSite}>{details.officialSite}</a></td>
-                             </tr>
-                             </tbody>
-                             <tbody>
-                             <tr>
-                                <td className="InsightHeader">Status</td>
-                                <td><span className={details.status === "Ended" ? "EndStatus" : "OngoingStatus"}>{details.status}</span></td>
-                             </tr>
-                             </tbody>
-                        </table>
+                                <tr>
+                                    <td className="InsightHeader">Language</td>
+                                    <td>{details.language}</td>
+                                </tr>
+                                </tbody>
+                                <tbody>
+                                <tr>
+                                    <td className="InsightHeader">Official Site</td>
+                                    <td><a href={details.officialSite}>{details.officialSite}</a></td>
+                                 </tr>
+                                 </tbody>
+                                 <tbody>
+                                 <tr>
+                                    <td className="InsightHeader">Status</td>
+                                    <td><span className={details.status === "Ended" ? "EndStatus" : "OngoingStatus"}>{details.status}</span></td>
+                                 </tr>
+                                 </tbody>
+                            </table>
 
-                    <div className="CastStrip">
-                        <h3>Cast</h3>
-                        <HorizontalCastList showId={details.id}></HorizontalCastList>
-                    </div>
+                        <div className="CastStrip">
+                            <h3>Cast</h3>
+                            <HorizontalCastList showId={details.id}></HorizontalCastList>
+                        </div>
+                     </div>
                  </div>
+                    {details.summary && <div className="ShowSummary">{renderHTML(details.summary)}</div>}
              </div>
-
         </Modal>);
 }
 
